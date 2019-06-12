42bed7919e9f3d693ff266e417de78971dd28455
ravi <ravisongare@promactinfo.com>
2019-06-12 17:41:11 +0530

    Assignment 1 Commit

---
+++ Assignment1/Employee.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Employee
+    {
+        public Employee( int id,string name,string departmentName)
+        {
+            this.Id = id;
+            Name = name;
+            DepartmentName = departmentName;
+        }
+        
+        private int Id;
+        private string Name;
+        private string DepartmentName;
+
+        public delegate void Event(string s);
+
+        public event Event Event1 = EventFunction;
+
+        //  Event Event1 = new Event(EventFunction);
+
+        public int GetId()
+        {
+            EventFunction("GetId()");
+            return Id;
+
+        }
+        public string GetName()
+        {
+            EventFunction("GetName()");
+            return Name;
+        }
+        public string GetDepartmentName()
+        {
+            EventFunction("GetDepartmentName()");
+            return DepartmentName;
+        }
+
+       static public void EventFunction(string s)
+        {
+            
+            Console.WriteLine($" {s} method called");
+        }
+
+        public void overloaded_method(int i)
+
+        {
+
+            Id = i;
+
+        }
+
+        public void overloaded_method(string i)
+
+        {
+
+            Name =i;
+
+        }
+
+        public void overloaded_method(int i, string s)
+
+        {
+
+            Id = i;
+
+            DepartmentName = s;
+
+        }
+    }
+}
---
+++ Assignment1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Id;
+            string Name;
+            string DepartmentName;
+
+        Console.WriteLine("enter id");
+            Id=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter name");
+            Name=Console.ReadLine();
+
+            Console.WriteLine("Enter department name");
+            DepartmentName= Console.ReadLine();
+
+            Employee emp = new Employee(Id, Name, DepartmentName);
+            Console.WriteLine(emp.GetId());
+            Console.WriteLine(emp.GetName());
+            Console.WriteLine(emp.GetDepartmentName());
+            Console.ReadLine();
+            emp.overloaded_method(5);
+        }
+    }
+}
